b645af12fda92c1eeb223a440bdf7a2066e3718c
110078 <szczygiel16@o2.pl>
2016-04-13 17:48:41 +0200

    Funkcja get_info gotowa do testowania

---
+++ kalkulator.py
@@ -0,0 +1,15 @@
+def dodawanie(a, b):
+	return a + b
+
+def get_info()
+	print("Program kalkulator. Autor: Aron")
+
+get_info()
+
+try:
+	l1 = int(input())
+	l2 = int(input())
+	print(dodawanie(l1, l2))
+except:
+	print("Program zakonczyl sie nieoczekiwanym bledem")
+	print("Mozesz to zglosic pod adresem pomoc@autor.pl")
